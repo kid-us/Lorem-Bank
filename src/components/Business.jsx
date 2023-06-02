4e166a04c7c713d0311955400199dd2f6bb4f06e
Kidus <98009847+kid-us@users.noreply.github.com>
2023-06-02 11:24:29 +0300

    Update Business.jsx

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -8,7 +8,7 @@ const Business = () => {
             <div className={layout.sectionInfo}>
                 <h2 className={styles.heading2}>You do the business, <br className="sm:block hidden" /> we'll handle the money.</h2>
                 <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-                    With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the mar`ke`t.
+                    With the right credit card, you can improve your financial life by building credit, earning rewards and saving money. But with hundreds of credit cards on the market.
                 </p>
 
                 <Button styles="mt-10" />
